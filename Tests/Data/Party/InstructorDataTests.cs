44a9df4415f49a252dee72b7d0b654bd5c759dbc
Ken Metsis <kenmetsis01@gmail.com>
2022-02-09 11:27:25 +0200

    Instructor testid

---
+++ Tests/Data/Party/InstructorDataTests.cs
@@ -0,0 +1,14 @@
+﻿using App.Data.Party;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace App.Tests.Data.Party
+{
+    [TestClass] public class InstructorDataTests:BaseTests<InstructorData> {
+        [TestMethod] public void IdTest() => isProperty<string>();
+        [TestMethod] public void FirstNameTest() => isProperty<string?>();
+        [TestMethod] public void LastNameTest() => isProperty<string?>();
+        [TestMethod] public void PhoneNrTest() => isProperty<string?>();
+        [TestMethod] public void LessonsGivenTest() => isProperty<string?>();
+
+    }
+}
---
+++ Tests/Facade/Party/InstructorViewTests.cs
@@ -0,0 +1,21 @@
+﻿using App.Facade.Party;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Tests.Facade.Party
+{
+    [TestClass]
+    public class InstructorViewTests:BaseTests<InstructorView>
+    {
+        [TestMethod] public void IdTest() => isProperty<string>();
+        [TestMethod] public void FirstNameTest() => isProperty<string?>();
+        [TestMethod] public void  LastNameTest() => isProperty<string?>();
+        [TestMethod] public void PhoneNrTest() => isProperty<string?>();
+        [TestMethod] public void LessonsGivenTest() => isProperty<string?>();
+        [TestMethod] public void FullNameTest() => isProperty<string?>();
+    }
+}
