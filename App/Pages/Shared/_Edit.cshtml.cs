8926d5c7cda0e3cb60dd157c331056f0d57582b9
Alar Laanep <alar.laanep@ttu.ee>
2022-02-10 10:48:27 +0200

    Git edit partial and layout vol2

---
+++ App/Pages/Shared/_Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace App.Pages.Shared
+{
+    public class _EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
