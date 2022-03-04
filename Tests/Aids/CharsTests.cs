1f5645b1a40b15ee91b80736b800cc401e076c0e
Alar Laanep <alar.laanep@ttu.ee>
2022-03-04 16:27:46 +0200

    Testklass, mis kontrollib, et kõik klassi liikmed oleksid testitudURL

--- Tests/Aids/CharsTests.cs
+++ Tests/Aids/CharsTests.cs
@@ -1,6 +1,15 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using App.Aids;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace App.Tests.Aids
 {
-    [TestClass] public class CharsTests: IsTypeTested  {   }
+    [TestClass] public class CharsTests: IsTypeTested  {
+        [TestMethod] public void IsNameCharTest() {
+            Assert.IsTrue(Chars.IsNameChar('a'));
+            Assert.IsTrue(Chars.IsNameChar('9'));
+            Assert.IsTrue(Chars.IsNameChar('.'));
+            Assert.IsTrue(Chars.IsNameChar('_'));
+            Assert.IsFalse(Chars.IsNameChar(':'));
+        }
+    }
 }
--- Tests/Aids/GetAssemblyTests.cs
+++ Tests/Aids/GetAssemblyTests.cs
@@ -2,5 +2,13 @@
 
 namespace App.Tests.Aids
 {
-    [TestClass] public class GetAssemblyTests : IsTypeTested { }
+    [TestClass]
+    public class GetAssemblyTests : IsTypeTested
+    {
+        [TestMethod]
+        public void ByNameTest() => inconclusive();
+        [TestMethod] public void OfTypeTest() => inconclusive();
+        [TestMethod] public void TypesTest() => inconclusive();
+        [TestMethod] public void TypeTest() => inconclusive();
+    }
 }
