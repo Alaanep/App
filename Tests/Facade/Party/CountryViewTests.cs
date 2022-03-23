296f85c0686dce0b2e2979b85f77845138b35eae
Alar Laanep <alar.laanep@ttu.ee>
2022-03-23 10:02:37 +0200

    countrydata, countryview, countryviewfactory testid

---
+++ Tests/Facade/Party/CountryViewTests.cs
@@ -0,0 +1,11 @@
+﻿using App.Facade.Party;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace App.Tests.Facade.Party {
+    [TestClass]
+    public class CountryViewTests: SealedClassTests<CountryView> {
+        [TestMethod] public void NameTest() => isProperty<string>();
+        [TestMethod] public void CodeTest() => isProperty<string>();
+        [TestMethod] public void DescriptionTest() => isProperty<string>();
+    }
+}
--- Tests/Facade/Party/InstructorViewFactoryTests.cs
+++ Tests/Facade/Party/InstructorViewFactoryTests.cs
@@ -16,14 +16,13 @@ namespace App.Tests.Facade.Party {
             isNotNull(v);
             isNotNull(v);
             //Vaja mõelda meetod, mis kontrollib, propertid kas on võrdsed
-            arePropertiesEqual(v, e, nameof(v.FullName));
-            //arePropertiesEqual(v, e, nameof(v.FullName)); //
-            //areEqual(v.Id, e.Id);
-            //areEqual(v.FirstName, e.FirstName);
-            //areEqual(v.LastName, e.LastName);
-            //areEqual(v.PhoneNr, e.PhoneNr);
-            //areEqual(v.LessonsGiven, e.LessonsGiven);
-            //areEqual(v.FullName, e.ToString());
+            //arePropertiesEqual(v, e, nameof(v.FullName));
+            areEqual(v.Id, e.Id);
+            areEqual(v.FirstName, e.FirstName);
+            areEqual(v.LastName, e.LastName);
+            areEqual(v.PhoneNr, e.PhoneNr);
+            areEqual(v.LessonsGiven, e.LessonsGiven);
+            areEqual(v.FullName, e.ToString());
         }
         [TestMethod]
         public void CreateEntityTest() {
@@ -32,14 +31,13 @@ namespace App.Tests.Facade.Party {
             isNotNull(e);
             isNotNull(v);
             //Vaja mõelda meetod, mis kontrollib, kas propertid on võrdsed
-            //arePropertiesEqual(v,e)
             arePropertiesEqual(e, v, nameof(e.Data));
-            //areEqual(e.Id, v.Id);
-            //areEqual(e.FirstName, v.FirstName);
-            //areEqual(e.LastName, v.LastName);
-            //areEqual(e.PhoneNr, v.PhoneNr);
-            //areEqual(e.LessonsGiven, v.LessonsGiven);
-            //areNotEqual(e.ToString(), v.FullName);
+            areEqual(e.Id, v.Id);
+            areEqual(e.FirstName, v.FirstName);
+            areEqual(e.LastName, v.LastName);
+            areEqual(e.PhoneNr, v.PhoneNr);
+            areEqual(e.LessonsGiven, v.LessonsGiven);
+            areNotEqual(e.ToString(), v.FullName);
         }
 
     }
